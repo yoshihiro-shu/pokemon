cb939dfd234f199b5a9aab41241afa6aca981c91
yoshihiroshu <yoshihiro.shu@zuuonline.com>
2022-10-09 23:05:15 +0900

    add axios

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -18,7 +18,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    // "@/plugins/axios",
+    '@/plugins/axios-accessor',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
